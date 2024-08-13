4c0f5943b7dfc5ee167d2add5b12372410b1d198
Md Ariful Islam Palash <Md Ariful Islam Palash>
2024-08-13 08:07:07 +0600

    implement the store, apiSlice and getting data from server

--- client-site/src/components/videos/Videos.jsx
+++ client-site/src/components/videos/Videos.jsx
@@ -1,15 +1,17 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+  const { data: videos, isLoading, isError, error } = useGetVideosQuery();
+
+  let content = null;
+  if (isLoading) content = <VideoLoader length={length} />;
+  if (!isLoading && isError) content = <Error message={error} />;
+  if (!isLoading && !isError && videos?.length === 0)
+    content = <Error message="No video Found!!" />;
+  if (!isLoading && !isError && videos?.length > 0)
+    content = videos?.map((video) => <Video key={video?.id} video={video} />);
+  return <>{content}</>;
 }
---
+++ client-site/src/features/api/apiSlice.js
@@ -0,0 +1,14 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9000" }),
+  endpoints: (builder) => ({
+    getVideos: builder.query({
+      query: () => "/videos",
+    //   keepUnusedDataFor: 5, // [ses]- how long to keep cached data in the store after the last subscriber stops using it.
+    }),
+  }),
+});
+
+export const {useGetVideosQuery} = apiSlice;
